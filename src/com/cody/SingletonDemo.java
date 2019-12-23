7df165329950610dac7508c801d1b4c0c7a9b2a5
cody <cody.meng@gizutech.com>
2019-12-23 19:49:55 +0800

    init project

---
+++ src/com/cody/SingletonDemo.java
@@ -0,0 +1,25 @@
+package com.cody;
+
+public class SingletonDemo {
+
+    /**
+     * 只能实例化一个类对象
+     * 将构造函数设置为私有的
+     *
+     */
+    private static SingletonDemo singletonDemo = new SingletonDemo();
+
+    private SingletonDemo() {
+    }
+
+    public static SingletonDemo getInstance() {
+        return singletonDemo;
+    }
+
+    public static void main(String[] args) {
+        SingletonDemo instance = SingletonDemo.getInstance();
+        System.out.println(instance);
+        SingletonDemo instance2 = SingletonDemo.getInstance();
+        System.out.println(instance2);
+    }
+}
